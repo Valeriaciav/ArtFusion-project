1173638eb6dee4b3e3b2cd53ee8f59061d9fe5ac
Roberto Morreale <roberto_morreale@hotmail.com>
2023-06-22 12:53:30 +0200

    eliminato seeder, modificata migration create categories e inserite categorie

--- database/migrations/2023_06_21_105340_create_categories_table.php
+++ database/migrations/2023_06_21_105340_create_categories_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\Category;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -16,8 +17,14 @@ return new class extends Migration
             $table->string('name');
             $table->timestamps();
         });
+        
+        $categories = ['Musica', 'Teatro', 'Cinema', 'Pittura', 'Fotografia', 'Scultura'];
 
-       
+        foreach($categories as $category){
+            Category::create([
+                'name' => $category,
+            ]);
+        }
 
     }
 
